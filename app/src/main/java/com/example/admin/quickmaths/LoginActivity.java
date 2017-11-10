ee5fd61236590f5834c941bf2715339afa11dba9
rgreen88 <rynelgreen@gmail.com>
2017-11-10 14:26:50 -0500

    corrected package and added FB share dialog
    - package renamed appropriately
    - LoginActivity can show ShareDialog
    - added some comments for reference

--- app/src/main/java/com/example/admin/quickmaths/LoginActivity.java
+++ app/src/main/java/com/example/admin/quickmaths/LoginActivity.java
@@ -1,10 +1,10 @@
-package com.example.admin.loginactivity;
+package com.example.admin.quickmaths;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.example.admin.quickmaths.R;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -12,7 +12,9 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.model.ShareLinkContent;
 import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
 import java.util.concurrent.Callable;
 
@@ -20,14 +22,21 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private static final String TAG = "Facebook Login";
+
+    //callback manager
     CallbackManager mFacebookCallbackManager;
 
+    //share dialog for share button
+    ShareDialog shareDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+        //initializing FB objects
         mFacebookCallbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
 
         // This MUST be placed after the above two lines.
         setContentView(R.layout.activity_login);
@@ -63,6 +72,14 @@ public class LoginActivity extends AppCompatActivity {
                 }
         );
 
+        //facebook showing ShareDialog
+        if (ShareDialog.canShow(ShareLinkContent.class)) {
+            ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+                    .build();
+            shareDialog.show(linkContent);
+        }
+
     }
 
     private void handleSignInResult(Callable<Void> callable) {
@@ -74,4 +91,14 @@ public class LoginActivity extends AppCompatActivity {
 /*
 ShareButton shareButton = (ShareButton)findViewById(R.id.fb_share_button);
 shareButton.setShareContent(content);
+
+
+getting url for content share:
+
+if (ShareDialog.canShow(ShareLinkContent.class)) {
+    ShareLinkContent linkContent = new ShareLinkContent.Builder()
+            .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
+            .build();
+    shareDialog.show(linkContent);
+}
  */
