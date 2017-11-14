43f2b7542292f679c72d95f2f072c8c9ca25fdd9
rgreen88 <rynelgreen@gmail.com>
2017-11-13 22:34:16 -0500

    updated LoginActivity and activity_login with functionality

--- app/src/main/java/com/example/admin/quickmaths/LoginActivity.java
+++ app/src/main/java/com/example/admin/quickmaths/LoginActivity.java
@@ -5,17 +5,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.share.model.ShareLinkContent;
-import com.facebook.share.widget.ShareButton;
-import com.facebook.share.widget.ShareDialog;
 
 import java.util.concurrent.Callable;
 
@@ -25,11 +28,10 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "Facebook Login";
 
     //callback manager
-    CallbackManager mFacebookCallbackManager;
+    private CallbackManager mFacebookCallbackManager;
 
-    //share dialog for share button
-    ShareDialog shareDialog;
-
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,61 +40,76 @@ public class LoginActivity extends AppCompatActivity {
 
         //initializing FB objects
         mFacebookCallbackManager = CallbackManager.Factory.create();
-        shareDialog = new ShareDialog(this);
-
-        // This MUST be placed after the above two lines.
+//        shareDialog = new ShareDialog(this);
         setContentView(R.layout.activity_login);
 
-        ShareButton shareButton = findViewById(R.id.facebook_share_button);
+        //image view and text view for login results
+        final ImageView imageView = findViewById(R.id.my_image_view);
+        final TextView textView = findViewById(R.id.tvWelcome);
+
+//        ShareButton shareButton = findViewById(R.id.facebook_share_button);
         //need to add content to object content for sharing
 
+        //token tracker
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            }
+        };
 
+        //tracking new logins from device
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                textView.setText(displayWelcomeMessage(currentProfile));
+            }
+        };
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
 
+        //request permission for retrieving user's friend's list and login btn
         LoginButton mFacebookSignInButton = findViewById(R.id.facebook_sign_in_button);
-        mFacebookSignInButton.registerCallback(mFacebookCallbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(final LoginResult loginResult) {
-                        //TODO: Use the Profile class to get information about the current user.
-                        handleSignInResult(new Callable<Void>() {
-                            @Override
-                            public Void call() throws Exception {
-                                LoginManager.getInstance().logOut();
-                                return null;
-                            }
-                        });
-                    }
+        mFacebookSignInButton.setReadPermissions("user_friends");
 
-                    @Override
-                    public void onCancel() {
-                        handleSignInResult(null);
-                    }
+        FacebookCallback<LoginResult> mCallBack = new FacebookCallback<LoginResult>() {
+            //retrieving user profile img and first name to welcome the user
+            @Override
+            public void onSuccess(final LoginResult loginResult) {
+                //TODO: Use the Profile class to get information about the current user.
 
-                    @Override
-                    public void onError(FacebookException error) {
-                        Log.d(LoginActivity.class.getCanonicalName(), error.getMessage());
-                        handleSignInResult(null);
-                    }
-                }
+                //retrieving access token and user profile
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                displayWelcomeMessage(profile);
 
-        );
+                //retrieving user profile img
+                Uri imageLink = profile.getProfilePictureUri(250, 250);
 
 
-        //facebook showing ShareDialog
-        if (ShareDialog.canShow(ShareLinkContent.class)) {
-            ShareLinkContent linkContent = new ShareLinkContent.Builder()
-                    .setContentUrl(Uri.parse("http://developers.facebook.com/android"))//example URL
-                    .build();
-            shareDialog.show(linkContent);
-            shareButton.setShareContent(linkContent);//TODO: add data to content for sharing
+                //Welcoming user via text view---removed if statement for now
+                // TODO:find why it returns null as if statement
 
-        }
+                Glide.with(LoginActivity.this)
+                        .load(imageLink)
+                        .into(imageView);
+                textView.setText("Welcome " + profile.getFirstName());
 
-//        MessageDialog.show(MainActivity.class, content);
-//        shareButton.setShareContent(linkContent);//TODO: add data to content for sharing
+            }
 
+            @Override
+            public void onCancel() {
+                handleSignInResult(null);
+            }
 
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(LoginActivity.class.getCanonicalName(), error.getMessage());
+                handleSignInResult(null);
+            }
+        };
 
+        //registering callback on login
+        mFacebookSignInButton.registerCallback(mFacebookCallbackManager, mCallBack);
     }
 
     //returning result from activity
@@ -107,5 +124,11 @@ public class LoginActivity extends AppCompatActivity {
         Log.d(TAG, "handleSignInResult: Signed in");
     }
 
-
+    private String displayWelcomeMessage(Profile profile) {
+        StringBuilder stringBuffer = new StringBuilder();
+        if (profile != null) {
+            stringBuffer.append("Welcome ").append(profile.getName());
+        }
+        return stringBuffer.toString();
+    }
 }
