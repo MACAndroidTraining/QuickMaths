280fb4322491d14189813652aa959735a0c44920
rgreen88 <rynelgreen@gmail.com>
2017-11-15 17:52:35 -0500

    refactor checkpoint 2

--- app/src/main/java/com/example/admin/quickmaths/view/placesActivity/PlacesActivity.java
+++ app/src/main/java/com/example/admin/quickmaths/view/placesActivity/PlacesActivity.java
@@ -5,8 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.admin.quickmaths.R;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.widget.LoginButton;
 import com.facebook.share.widget.ShareButton;
 
 public class PlacesActivity extends AppCompatActivity {
@@ -17,11 +26,51 @@ public class PlacesActivity extends AppCompatActivity {
     String shareBody = "This is a great app! You should try it now!";
     ShareButton shareButton;
 
+    private static final String TAG = "Facebook Login";
+
+    //callback manager
+    private CallbackManager mFacebookCallbackManager;
+
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        //btnShare Bind
+        btnShare = findViewById(R.id.btnShare);
+
+        //initializing FB objects
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+
+        //image view and text view for login results
+        final ImageView imageView = findViewById(R.id.my_image_view);
+        final TextView textView = findViewById(R.id.tvWelcome);
+
+        //token tracker
+        mTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            }
+        };
+
+        //tracking new logins from device
+        mProfileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                textView.setText(displayWelcomeMessage(currentProfile));
+            }
+        };
+        mTokenTracker.startTracking();
+        mProfileTracker.startTracking();
+
+        //request permission for retrieving user's friend's list and login btn
+        LoginButton mFacebookSignInButton = findViewById(R.id.facebook_sign_in_button);
+        mFacebookSignInButton.setReadPermissions("user_friends");
     }
 
     public void btnShare(View view) {
